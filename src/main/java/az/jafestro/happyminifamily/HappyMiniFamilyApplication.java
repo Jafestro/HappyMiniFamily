b2137cd88c8dda5863e0669a34d81e50f7f5e5ec
Jafar Jafarov <jafestro@gmail.com>
2023-04-20 16:52:54 +0400

    Initial commit

---
+++ src/main/java/az/jafestro/happyminifamily/HappyMiniFamilyApplication.java
@@ -0,0 +1,13 @@
+package az.jafestro.happyminifamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMiniFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMiniFamilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/jafestro/happyminifamily/HappyMiniFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.jafestro.happyminifamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMiniFamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
